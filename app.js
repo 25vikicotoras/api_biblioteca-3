fac7f2b51070c57e502cf68a2b86b0f6e884d541
25vikicotoras <vikicotoras@gmail.com>
2024-01-17 22:41:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const { auth } = require("express-oauth2-jwt-bearer");
+const errorHandler = require("./middleware/errorHandler.js");
+
+//Configuracion Middleware con el Servidor de Autorización
+const autenticacion = auth({
+    audience: "http://localhost:3000/api/productos",
+    issuerBaseURL: "http://dev-utn-frc-iaew.auth0.com/",
+    tokenSigningAlg: "RS256",
+});
+
+const app = express();
+app.use(express.json());
+
+//Importamos el Router de Libos
+const LibrosRouter = require('./routes/libros.js');
+
+//Configuramos el middleware de autenticacion
+app.use("/libros", autenticacion, LibrosRouter);
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+    console.log('servidor iniciado en el puerto 3000');
+});
---
+++ middleware/errorHandler.js
@@ -0,0 +1,20 @@
+const { modelName } = require("../models/libro");
+
+//middleware/errorHandler.js
+const errorHandler = (err,req,res,next) => {
+    //verificar si el error tiene un código de estado definido, de lo contrario, establecer el código de estado predeterminado
+    const statusCode = err.statusCode || 500;
+
+    //Construir objeto de respuesta de error
+    const errorResponse = {
+        error: {
+            message: err.message || "Error interno del servidor",
+            code: err.code || "internal_error",
+        },
+    };
+
+    //Enviar respuesta de error en formato JSON
+    res.status(statusCode).json(errorResponse);
+};
+
+module.exports = errorHandler; 
